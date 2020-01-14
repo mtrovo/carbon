6c8c72a74d17a29ac3e99c19abedcec593afbfca
Moises Trovo <moises.trovo@careem.com>
2020-01-14 15:09:42 +0100

    embed locale data into lang package

--- lang/loader.go
+++ lang/loader.go
@@ -1,16 +1,22 @@
 package lang
 
-import (
-	"errors"
-	"io/ioutil"
+import "fmt"
+
+var (
+	locales map[string]map[string]string
 )
 
+func init()  {
+	locales = make(map[string]map[string]string)
+	locales["en"] = en
+}
+
 // LoadLocaleText will load the translations from the locale json files according to the locale
-func LoadLocaleText(l string) ([]byte, error) {
-	lText, err := ioutil.ReadFile("./lang/" + l + ".json")
-	if err != nil {
-		return nil, errors.New("not able to read the lang file:" + err.Error())
+func LoadLocaleText(l string) (map[string]string, error) {
+	locale, ok := locales[l]
+	if !ok {
+		return nil, fmt.Errorf("locale data not available: %s", l)
 	}
 
-	return lText, nil
+	return locale, nil
 }
